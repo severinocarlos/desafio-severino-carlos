85cb9e5fffcec3397ca85d6eb16bd8628fee4bd7
severinocarlos <juninho_456@hotmail.com>
2023-08-16 13:10:52 -0300

    function adjustments and adding price calculation

--- src/caixa-da-lanchonete.js
+++ src/caixa-da-lanchonete.js
@@ -1,44 +1,77 @@
 class Menu {
     getMenu() {
         return  {
-                'cafe': {principalDish: true, price: 3.00},
-                'chantily': {principalDish: false, price: 1.50},
-                'suco': {principalDish: true, price: 6.20},
-                'sanduiche': {principalDish: true, price: 6.50},
-                'queijo': {principalDish: false, price: 2.00},
-                'salgado': {principalDish: true, price: 7.25},
-                'combo1': {principalDish: true, price: 9.50},
-                'combo2': {principalDish: true, price: 7.50},
+                'cafe': {description: 'cafe', price: 3.00},
+                'chantily': {description: 'cafe', price: 1.50},
+                'suco': {description: 'suco', price: 6.20},
+                'sanduiche': {description: 'sanduiche', price: 6.50},
+                'queijo': {description: 'sanduiche', price: 2.00},
+                'salgado': {description: 'salgado', price: 7.25},
+                'combo1': {description: 'combo1', price: 9.50},
+                'combo2': {description: 'combo2', price: 7.50},
         } 
     }
 }
 
 class Payment {
-    
+    getDiscount(paymentMethod, price) {
+        if (paymentMethod == 'dinheiro') {
+            price -= price * 5 / 100
+        } else if (paymentMethod == 'credito') {
+            price += price * 3 / 100
+        }
+
+        return price
+    }
 }
+
 class CaixaDaLanchonete {
 
     calcularValorDaCompra(metodoDePagamento, itens) {
         const orderList = this.getOrderList(itens)
+        const message = this.orderIsRight(metodoDePagamento, orderList)
 
-        const message = this.orderIsRight(orderList)
+        if (message) {
+            return message
+        } else {
+            let price = 0
+            const menu = new Menu().getMenu()
+            orderList.forEach(item => {
+                price += (menu[item.order].price * item.quantity)
+            })
 
-        
-
-
-        return "";
+            price = new Payment().getDiscount(metodoDePagamento, price)
+            return `R$ ${price.toFixed(2).replace('.', ',')}`
+        }
+    
     }
     
     orderIsRight(paymentMethod, items) {
         if (this.cartIsEmpty(items)) {
             return "Não há itens no carrinho de compra!"
-        } else if (!this.itemExist(items)) {
+        } else if (!(this.itemExist(items))) {
             return "Item inválido!"
         } else if (!(this.quantityIsValid(items))) {
             return "Quantidade inválida!"
         } else if (!(this.IsFormOfPayment(paymentMethod))) {
             return "Forma de pagamento inválida!"
+        } else if (!this.checkExtraWithPrincipal(items)) {
+            return "Item extra não pode ser pedido sem o principal"
         }
+
+    }
+
+    checkExtraWithPrincipal(items) {
+        let id = true
+        items.forEach(item => {
+            const menu = new Menu().getMenu()
+            if (item != menu[item.order].description) {
+                if (!items.find(order => order.order == menu[item.order].description)) {
+                    id = false
+                }
+            }
+        })
+        return id
     }
 
     IsFormOfPayment(paymentMethod) {
@@ -46,23 +79,28 @@ class CaixaDaLanchonete {
     }
 
     quantityIsValid(items) {
+        let valid = true
         items.forEach(item => {
-            if (item.quantity == 0) return item.quantity
-            
+            if (item.quantity == "0") {
+                valid = false
+            }
         });
+        return valid
     }
 
     itemExist(items) {
+        let exist = true
         items.forEach(item => {
             const menu = new Menu().getMenu()
-            if (!menu[item.order]) {
-                return false
+            if (!(menu[item.order])) {
+                exist = false
             }
         })
+        return exist
     }
     
     cartIsEmpty(items) {
-        return items.lenght == 0
+        return items.length == 0
     }
 
     getOrderList(items) {
@@ -73,5 +111,5 @@ class CaixaDaLanchonete {
     }
 
 }
-console.log(new CaixaDaLanchonete().calcularValorDaCompra('debito', ['cafe,1','chantily,1']))
+
 export { CaixaDaLanchonete };
